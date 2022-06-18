e46e897c9f1e16c692cdedc201ec6e0ddd55f47a
ansel brandt <ansel@anselbrandt.com>
2022-06-18 06:42:39 -0400

    Use generated imageHash

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,6 @@ import {
 import decode from "./decode";
 import encode from "./encode";
 
-const hash = "U9GIY:ISwhMv*GJDD6E3?tyEDOtS?ZW@8{Rk";
-
 async function main() {
   const image = await loadImage("./public/limecat.jpg");
   const { naturalWidth: width, naturalHeight: height } = image;
@@ -24,7 +22,7 @@ async function main() {
     4
   );
   console.log(imageHash);
-  const decoded = decode(hash, width, height);
+  const decoded = decode(imageHash, width, height);
   const newImage = createImageData(decoded, width, height);
 
   const newCanvas = createCanvas(width, height);
