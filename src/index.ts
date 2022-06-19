9d3212a8cea0fc5a90e5f219db597756c50aa798
ansel brandt <ansel@anselbrandt.com>
2022-06-18 21:57:19 -0400

    Add resize

--- src/index.ts
+++ src/index.ts
@@ -7,31 +7,56 @@ import {
 import decode from "./decode";
 import encode from "./encode";
 
-async function main() {
-  const image = await loadImage("./public/limecat.jpg");
-  const { naturalWidth: width, naturalHeight: height } = image;
-  const canvas = createCanvas(width, height);
-  const ctx = canvas.getContext("2d");
-  ctx.drawImage(image, 0, 0, width, height);
-  const imageData = ctx?.getImageData(0, 0, image.width, image.height);
-  const imageHash = encode(
-    imageData.data,
-    imageData.width,
-    imageData.height,
-    4,
-    4
-  );
-  console.log(imageHash);
-  const decoded = decode(imageHash, width, height);
-  const newImage = createImageData(decoded, width, height);
+interface Size {
+  width: number;
+  height: number;
+  newWidth?: number;
+}
+const resize = ({ width, height, newWidth = 32 }: Size) => {
+  if (width <= 32) return { width, height };
+  const scaledHeight = Math.round((newWidth / width) * height);
+  return { width: newWidth, height: scaledHeight };
+};
 
+const hashGen = async (filename: string) => {
+  const image = await loadImage(`./public/${filename}`);
+  const { naturalWidth: width, naturalHeight: height } = image;
+  const { width: scaledWidth, height: scaledHeight } = resize({
+    width,
+    height,
+  });
+  const canvas = createCanvas(scaledWidth, scaledHeight);
+  const ctx = canvas.getContext("2d");
+  ctx.drawImage(image, 0, 0, scaledWidth, scaledHeight);
+  const imageData = ctx?.getImageData(0, 0, scaledWidth, scaledHeight);
+  const hash = encode(imageData.data, scaledWidth, scaledHeight, 4, 4);
+  return { hash, width: scaledWidth, height: scaledHeight, filename };
+};
+
+interface Options {
+  hash: string;
+  width: number;
+  height: number;
+  filename: string;
+}
+
+const imageGen = async ({ hash, width, height, filename }: Options) => {
+  const decoded = decode(hash, width, height);
+  const newImage = createImageData(decoded, width, height);
   const newCanvas = createCanvas(width, height);
   const newCtx = newCanvas.getContext("2d");
   newCtx.putImageData(newImage, 0, 0);
-  const out = fs.createWriteStream(`./public/out.png`);
-  const stream = newCanvas.createPNGStream();
+  const newFilename = filename.split(".").slice(0, -1).join(".") + "-blur.jpg";
+  const out = fs.createWriteStream(`./public/${newFilename}`);
+  const stream = newCanvas.createJPEGStream();
   stream.pipe(out);
-  out.on("finish", () => console.log("The PNG file was created."));
+  out.on("finish", () => console.log(`${hash} -> ${newFilename}`));
+};
+
+async function main() {
+  const imageHash = await hashGen("limecat.jpg");
+  console.log(imageHash);
+  await imageGen(imageHash);
 }
 
 main().catch((error) => console.log(error));
