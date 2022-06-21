671ada789b3bc311456878e59359b9aacde378ab
ansel brandt <ansel@anselbrandt.com>
2022-06-21 19:03:46 -0400

    Add scale param

--- src/index.ts
+++ src/index.ts
@@ -11,26 +11,29 @@ interface Size {
   width: number;
   height: number;
   newWidth?: number;
+  scale?: boolean;
 }
-const resize = ({ width, height, newWidth = 32 }: Size) => {
+const resize = ({ width, height, newWidth = 32, scale = true }: Size) => {
+  if (!scale) return { width, height };
   if (width <= 32) return { width, height };
   const scaledHeight = Math.round((newWidth / width) * height);
   return { width: newWidth, height: scaledHeight };
 };
 
-const hashGen = async (filename: string) => {
+const hashGen = async (filename: string, scale = true) => {
   const image = await loadImage(`./public/${filename}`);
   const { naturalWidth: width, naturalHeight: height } = image;
   const { width: scaledWidth, height: scaledHeight } = resize({
     width,
     height,
+    scale,
   });
   const canvas = createCanvas(scaledWidth, scaledHeight);
   const ctx = canvas.getContext("2d");
   ctx.drawImage(image, 0, 0, scaledWidth, scaledHeight);
   const imageData = ctx?.getImageData(0, 0, scaledWidth, scaledHeight);
   const hash = encode(imageData.data, scaledWidth, scaledHeight, 4, 4);
-  return { hash, width, height, filename };
+  return { hash, width, height, filename, scale };
 };
 
 interface Options {
@@ -38,14 +41,15 @@ interface Options {
   width: number;
   height: number;
   filename: string;
+  scale?: boolean;
 }
 
-const imageGen = async ({ hash, width, height, filename }: Options) => {
+const imageGen = async ({ hash, width, height, filename, scale }: Options) => {
   const { width: scaledWidth, height: scaledHeight } = resize({
     width,
     height,
+    scale,
   });
-
   const decoded = decode(hash, scaledWidth, scaledHeight);
   const newImage = createImageData(decoded, scaledWidth, scaledHeight);
   const newCanvas = createCanvas(scaledWidth, scaledHeight);
@@ -59,7 +63,7 @@ const imageGen = async ({ hash, width, height, filename }: Options) => {
 };
 
 async function main() {
-  const imageHash = await hashGen("limecat.jpg");
+  const imageHash = await hashGen("charmaine.jpg");
   console.log(imageHash);
   await imageGen(imageHash);
 }
